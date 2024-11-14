a28f4a60485231c2c2e35ced32ab49fd237cee43
nauftis <182428560+Nauftis@users.noreply.github.com>
2024-11-14 10:16:20 +0100

    Change html in order to get buttons in right order and stylize css doc

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function add(a, b) {
     return a + b;
 }
 
-function substract(a, b) {
+function subtract(a, b) {
     return a - b;
 }
 
@@ -10,25 +10,59 @@ function multiply(a, b) {
     return a * b;
 }
 
-function devide(a, b) {
+function divide(a, b) {
     return a / b;
 }
 
-const num1 = ""
-const operator = ""
-const num2 = ""
+let num1 = ""
+let operator = ""
+let num2 = ""
 
 function operate(opp, a, b) {
     switch (opp) {
         case '+':
             return add(a, b);
         case '-':
-            return substract(a, b);
+            return subtract(a, b);
         case '*':
             return multiply(a, b);
         case '/':
-            return devide(a, b);
+            return divide(a, b);
         default:
             return null;
     }
 }
+const display = document.getElementById("display")
+
+for (let i = 0; i < 10; i++) {
+   const inputNumber = document.getElementById("numPad" + i);
+    inputNumber.addEventListener("click", () => {
+        addNumberToCalculation(i);
+        
+    });
+}
+
+function addNumberToCalculation(number) {
+    num1 += number.toString();
+    showOnDisplay();
+}
+
+for (let i = 0; i < 4; i++) {
+    const inputOperator = document.getElementById("operator" + i);
+     inputOperator.addEventListener("click", () => {
+         addOperatorToCalculation(i);
+         
+     });
+ }
+
+function addOperatorToCalculation(op) {
+    alert(`Operator is ${op}`)
+}
+
+function showOnDisplay() {
+    if (operator == null) {
+        display.textContent = `${num1}`;
+    } else {
+        display.textContent = `${num1} ${operator} ${num2}`;
+    }
+};
